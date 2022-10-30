42e2f76cf18f5c53406c9b56ca7417a44c92b93b
Jacek <jacik@toya.net.pl>
2022-10-30 23:30:59 +0100

    select mp3

---
+++ app/src/main/java/jacek/brzezinski/piktogramy/AudioModel.java
@@ -0,0 +1,49 @@
+package jacek.brzezinski.piktogramy;
+
+public class AudioModel {
+    private int id;
+    private String name;
+    private String path;
+
+    public AudioModel(int id, String name, String path) {
+        this.id = id;
+        this.name = name;
+        this.path = path;
+    }
+
+    public AudioModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return "AudioModel{" +
+                "id=" + id +
+                ", name='" + name + "'" +
+                ", path='" + path + "'" +
+                '}';
+    }
+}
--- app/src/main/java/jacek/brzezinski/piktogramy/PictogramAddActivity.java
+++ app/src/main/java/jacek/brzezinski/piktogramy/PictogramAddActivity.java
@@ -2,66 +2,61 @@ package jacek.brzezinski.piktogramy;
 
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.Adapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class PictogramAddActivity extends AppCompatActivity {
+    public static final int PICK_IMAGE_CAMERA = 5;
     public static final int PICK_IMAGE_FILE = 10;
-    public static final int PICK_IMAGE_CAMERA = 20;
+    public static final int PICK_AUDIO_FILE = 20;
+
     private ImageView imageView;
-    private File audioFile;
+    private FloatingActionButton actionButtonPlay;
+
+    private Uri audioFile;
+    private Bitmap imageFile;
     private PictogramModel pictogramModel = new PictogramModel();
 
-    private Uri outputFileUri;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pictogram_add);
         imageView = (ImageView) findViewById(R.id.pictogramAddIcon);
+        actionButtonPlay = (FloatingActionButton) findViewById(R.id.ActionButtonPlay);
         pictogramModel.setPath("p_test");
-
-    }
-
-    public void actionPhotoFromCamera(View view) {
-//        Intent intent = new Intent();
-//        intent.setType("image/*");
-//        intent.setAction(Intent.ACTION_GET_CONTENT);
-//        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_CAMERA);
-//        File file = new File(Environment.getExternalStorageDirectory(),
-//                "p_" + UUID.randomUUID() + ".jpg");
-        File file = PictogramAddActivity.this.getExternalFilesDir(UUID.randomUUID() + ".jpg");
-        Toast.makeText(this, "file " + file, Toast.LENGTH_SHORT).show();
-
-        outputFileUri = FileProvider.getUriForFile(PictogramAddActivity.this, BuildConfig.APPLICATION_ID + ".fileprovider", file);
-        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-        //takePhotoIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        //takePhotoIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File("/sdcard/tmp")));
-
-
-        //Intent chooseImageIntent = ImagePicker.getPickImageIntent(PictogramAddActivity.this);
-        startActivityForResult(takePhotoIntent, PICK_IMAGE_CAMERA);
     }
 
     public void actionPhotoFromFile(View view) {
@@ -75,16 +70,15 @@ public class PictogramAddActivity extends AppCompatActivity {
     }
 
     public void actionAudioFromFile(View view) {
-//        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
-//        getIntent.setType("image/*");
-//        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//        pickIntent.setType("image/*");
-//        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
-//        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
-//        startActivityForResult(chooserIntent, PICK_IMAGE_FILE);
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        //intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("audio/*"); // specify "audio/mp3" to filter only mp3 files
+        startActivityForResult(intent, PICK_AUDIO_FILE);
     }
 
     public void actionAudioPlay(View view) {
+        mp = MediaPlayer.create(PictogramAddActivity.this, audioFile);
+        mp.start();
 
     }
 
@@ -114,13 +108,23 @@ public class PictogramAddActivity extends AppCompatActivity {
                 try {
                     final Uri imageUri = data.getData();
                     final InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                    imageView.setImageBitmap(selectedImage);
+                    imageFile = BitmapFactory.decodeStream(imageStream);
+                    imageView.setImageBitmap(imageFile);
                     Toast.makeText(this, "RESULT FILE " + Integer.toString(resultCode), Toast.LENGTH_SHORT).show();
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                     Toast.makeText(PictogramAddActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
                 }
+            }
+            if (requestCode == PICK_AUDIO_FILE) {
+//                try {
+                final Uri audioUri = data.getData();
+                audioFile = audioUri;
+                actionButtonPlay.setEnabled(true);
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                    Toast.makeText(PictogramAddActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+//                }
             }
 
         }
