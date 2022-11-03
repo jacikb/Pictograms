651e833677cd36fd20298e89e26a1a10d3dadd2c
Jacek <jacik@toya.net.pl>
2022-11-03 20:29:34 +0100

    add pictograms about from preferences

--- app/src/main/java/jacek/brzezinski/piktogramy/AboutActivity.java
+++ app/src/main/java/jacek/brzezinski/piktogramy/AboutActivity.java
@@ -1,8 +1,13 @@
 package jacek.brzezinski.piktogramy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        TextView tv_title = findViewById(R.id.aboutTextViewTitle);
+        TextView tv_phone = findViewById(R.id.aboutTextViewPhone);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(AboutActivity.this);
+        tv_title.setText("Właściciel " + prefs.getString("owner", "..."));
+        tv_phone.setText(getResources().getString(R.string.about_author_phone) + " " + prefs.getString("phone", "..."));
+        if (!prefs.getString("phone", "...").equals("737 456 220")) {
+            ImageView iv_image = findViewById(R.id.imageViewPawel);
+            iv_image.setVisibility(View.INVISIBLE);
+        }
     }
 }
